02b51e41042f3c05a02add1582c529232af40da7
Phil Liao <cheweiphil@gmail.com>
2023-12-29 09:50:24 -0600

    System industrialized of running successfully.

--- client.py
+++ client.py
@@ -1,39 +1,63 @@
 import socket
 import threading
 
-# Choosing Nickname
-nickname = input("Choose your nickname: ")
+SERVER = '127.0.0.1'
+PORT = 7777
+HEADER = 1024
+FORMAT = 'ascii'
 
-# Connecting To Server
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 7777))
 
-# Listening to Server and Sending Nickname
-def receive():
-    while True:
-        try:
-            # Receive Message From Server
-            # If 'NICK' Send Nickname
-            message = client.recv(1024).decode('ascii')
-            if message == 'NICK':
-                client.send(nickname.encode('ascii'))
-            else:
-                print(message)
-        except:
-            # Close Connection When Error
-            print("An error occured!")
-            client.close()
-            break
 
-# Sending Messages To Server
-def write():
-    while True:
-        message = '{}: {}'.format(nickname, input(''))
-        client.send(message.encode('ascii'))
+KEYS = {'USERNAME_KEY':'USERNAME'}
+class conn:
+    def __init__(self, username:str, SERVER:str=SERVER, PORT:int=PORT, HEADER:int=HEADER, FORMAT:str=FORMAT, KEYS:dict[str, str]=KEYS) -> None:
+        self.username = username
+        self.SERVER = SERVER
+        self.PORT = PORT
+        self.HEADER = HEADER
+        self.FORMAT = FORMAT
+        self.KEYS = KEYS
+        self.client = self.start_connection()
+    def start_connection(self) -> socket.socket: # Start Connection to Server
+        _client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        _client.connect((self.SERVER, self.PORT))
+        return _client
+    def end_connection(self) -> None: # End Connection to Server
+        self.client.close()
+    def receive_from_server(self) -> str: # Receive Data from Server
+        _data = self.client.recv(self.HEADER)
+        _data = _data.decode(self.FORMAT)
+        return _data
+    def send_to_server(self, data:str) -> None: # Send Data to Server
+        data = data.encode(FORMAT)
+        self.client.send(data)
+    def handle(self): # Listening to Server and Sending Nickname
+        while True:
+            try:
+                message = self.receive_from_server()
+                if message == self.KEYS['USERNAME_KEY']:
+                    self.send_to_server(self.username)
+                else:
+                    print(message)
+            except:
+                # Close Connection When Error
+                print("An error occured!")
+                self.end_connection()
+                break
+    def write(self): # Inputting String Data
+        while True:
+            _message = '{}: {}'.format(username, input(''))
+            self.send_to_server(_message)
 
+
+
+
+username = input("Input username: ")
+
+connection = conn(username, SERVER, PORT, HEADER, FORMAT)
 # Starting Threads For Listening And Writing
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
+handle_thread = threading.Thread(target=connection.handle, )
+handle_thread.start()
 
-write_thread = threading.Thread(target=write)
+write_thread = threading.Thread(target=connection.write, )
 write_thread.start()
--- server.py
+++ server.py
@@ -1,56 +1,83 @@
 import socket
 import threading
+
 # Connection Data
-host = '127.0.0.1'
-port = 7777
+HOST = '127.0.0.1'
+PORT = 7777
+HEADER = 1024
+FORMAT = 'ascii'
 
-# Starting Server
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen()
 
-# Lists For Clients and Their Nicknames
+
+
+KEYS = {'USERNAME_KEY':'USERNAME'}
+
+class central_server:
+    def __init__(self, clients:list[socket.socket], usernames:list[str], HOST:str=HOST, PORT:int=PORT, HEADER:int=HEADER, FORMAT:str=FORMAT, KEYS:dict[str, str]=KEYS) -> None:
+        self.clients = clients
+        self.usernames = usernames
+        self.HOST = HOST
+        self.PORT = PORT
+        self.HEADER = HEADER
+        self.FORMAT = FORMAT
+        self.KEYS = KEYS
+        self.server = self.start_server()
+    def start_server(self) -> None: # Start the Server
+        _server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        _server.bind((self.HOST, self.PORT))
+        _server.listen()
+        return _server
+    def end_server(self) -> None: #End the Server
+        self.server.close()
+    def send_to_one(self, _client:socket.socket, _data:str) -> None: # Send Data to One Client
+        _data = _data.encode(self.FORMAT)
+        _client.send(_data)
+    def broadcast(self, _data:str) -> None: # Sending Data to All Connected Clients
+        _data = _data.encode(self.FORMAT)
+        for client in self.clients:
+            client.send(_data)
+    def receive(self, client:socket.socket) -> str: # Receiving and Decoding Data from Client
+        _data = client.recv(self.HEADER)
+        _data = _data.decode(self.FORMAT)
+        return _data
+    def remove_client(self, _client:socket.socket) -> None: # Removing And Closing Clients
+        _index = self.clients.index(_client)
+        self.clients.remove(_client)
+        _client.close()
+        _username = self.usernames[_index]
+        self.broadcast('{} left!'.format(self._username))
+        self.usernames.remove(_username)
+    def handle_client(self, _client:socket.socket) -> None: # Handling Messages from Clients
+        while True:
+            try:
+                _message = self.receive(_client)
+                self.broadcast(_message)
+            except:
+                self.remove_client(_client)
+                break
+    def add_client(self) -> None: # Listening Function
+        while True:
+            # Accept Connection
+            _client, _address = self.server.accept()
+            print("Connected with {}".format(str(_address)))
+
+            # Request And Store Nickname
+
+            self.send_to_one(_client, self.KEYS['USERNAME_KEY'])
+            _username = self.receive(_client)
+            self.usernames.append(_username)
+            self.clients.append(_client)
+
+            # Print And Broadcast Username
+            print("Username is {}".format(_username))
+            self.broadcast(f'{_username} joined!')
+            self.send_to_one(_client, 'Connected to server!')
+
+            # Start Handling Thread For Client
+            thread = threading.Thread(target=(self.handle_client), args=(_client,))
+            thread.start()  
+# Lists for Clients and Their Usernames
 clients = []
-nicknames = []
-# Sending Messages To All Connected Clients
-def broadcast(message):
-    for client in clients:
-        client.send(message)
-# Handling Messages From Clients
-def handle(client):
-    while True:
-        try:
-            # Broadcasting Messages
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            # Removing And Closing Clients
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            broadcast('{} left!'.format(nickname).encode('ascii'))
-            nicknames.remove(nickname)
-            break
-# Receiving / Listening Function
-def receive():
-    while True:
-        # Accept Connection
-        client, address = server.accept()
-        print("Connected with {}".format(str(address)))
-
-        # Request And Store Nickname
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
-        nicknames.append(nickname)
-        clients.append(client)
-
-        # Print And Broadcast Nickname
-        print("Nickname is {}".format(nickname))
-        broadcast("{} joined!".format(nickname).encode('ascii'))
-        client.send('Connected to server!'.encode('ascii'))
-
-        # Start Handling Thread For Client
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()        
-receive()
+usernames = []
+conn = central_server(clients, usernames)
+conn.add_client()
